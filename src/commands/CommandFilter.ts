7377866df588dafcff8b7c668aa652772af6d354
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-28 00:28:30 +0900

    filter removeコマンドを追加

--- src/commands/CommandFilter.ts
+++ src/commands/CommandFilter.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, CommandInteraction, CacheType } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 import { Command } from './Command';
 import { Filter } from '../filter/Filter';
 
@@ -22,56 +22,89 @@ export class CommandFilter implements Command {
                         .setDescription('出力')
                         .setRequired(true),
                 ),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('remove')
+                .setDescription('単語を削除します')
+                .addStringOption((option) =>
+                    option
+                        .setName('input')
+                        .setDescription('入力')
+                        .setRequired(true),
+                ),
         );
 
-    execute = async (interaction: CommandInteraction<CacheType>) => {
+    execute = async (interaction: ChatInputCommandInteraction) => {
         const guild = interaction.guild;
         if (guild == null) {
-            interaction.reply('このコマンドはセーバー内でのみ使用できます！');
-            return;
-        }
-        if (!interaction.isChatInputCommand()) {
+            await interaction.reply('このコマンドはセーバー内でのみ使用できます！');
             return;
         }
         const filter = await Filter.get(interaction.client, guild);
         const subcommand = interaction.options.getSubcommand();
         switch (subcommand) {
             case 'add':
-                {
-                    const input = interaction.options.getString('input', true);
-                    const output = interaction.options.getString(
-                        'output',
-                        true,
-                    );
-                    const success = await filter.addWord(input, output);
-                    if (success) {
-                        interaction.reply({
-                            embeds: [
-                                {
-                                    title: '単語を追加しました',
-                                    description:
-                                        'input: `' +
-                                        input +
-                                        '`\noutput: `' +
-                                        output +
-                                        '`',
-                                },
-                            ],
-                        });
-                    } else {
-                        interaction.reply({
-                            embeds: [
-                                {
-                                    title: 'その単語はすでに追加されています！',
-                                    description: 'input: `' + input + '`',
-                                },
-                            ],
-                        });
-                    }
-                }
+                await add(interaction, filter);
+                break;
+            case 'remove':
+                await remove(interaction, filter);
                 break;
             default:
-                interaction.reply(`不明なサブコマンドです: ${subcommand}`);
+                await interaction.reply(`不明なサブコマンドです: ${subcommand}`);
         }
     };
+}
+
+async function add(interaction: ChatInputCommandInteraction, filter: Filter) {
+    const input = interaction.options.getString('input', true);
+    const output = interaction.options.getString('output', true);
+    const success = await filter.addWord(input, output);
+    if (success) {
+        await interaction.reply({
+            embeds: [
+                {
+                    title: '単語を追加しました',
+                    description:
+                        'input: `' + input + '`\noutput: `' + output + '`',
+                },
+            ],
+        });
+    } else {
+        await interaction.reply({
+            embeds: [
+                {
+                    title: 'その単語はすでに追加されています！',
+                    description: 'input: `' + input + '`',
+                },
+            ],
+        });
+    }
+}
+
+async function remove(
+    interaction: ChatInputCommandInteraction,
+    filter: Filter,
+) {
+    const input = interaction.options.getString('input', true);
+    const success = await filter.removeWord(input);
+    if (success) {
+        await interaction.reply({
+            embeds: [
+                {
+                    title: '単語を削除しました',
+                    description: 'input: `' + input + '`',
+                },
+            ],
+        });
+    } else {
+        await interaction.reply({
+            embeds: [
+                {
+                    title: 'その単語は追加されていないか、すでに削除されています！',
+                    description: 'input: `' + input + '`',
+                },
+            ],
+        });
+    }
 }
